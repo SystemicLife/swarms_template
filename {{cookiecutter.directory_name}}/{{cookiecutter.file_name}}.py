af7ce3f91abd0092ed85c23645b1cb96030c1098
evelynmitchell <efmphone@gmail.com>
2023-10-04 13:01:45 -0600

    omnimodal per-prompt projects cookiecutter

--- {{cookiecutter.directory_name}}/{{cookiecutter.file_name}}.py
+++ {{cookiecutter.directory_name}}/{{cookiecutter.file_name}}.py
@@ -1,1 +1,9 @@
-print("Hello, {{cookiecutter.greeting_recipient}}!")
+from langchain.llms import OpenAIChat
+from swarms.agents import OmniModalAgent
+
+
+llm = OpenAIChat(model_name="gpt-4")
+
+agent = OmniModalAgent(llm)
+
+agent.run("{{cookiecutter.starting_prompt}}")
